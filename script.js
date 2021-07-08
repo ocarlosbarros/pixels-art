abc5fe3cc2e60bf8cf96aef2838e4a3057f04e86
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-08 18:15:34 -0300

    Adiciçao de comentarios para as funçoes createPixel e fillBoard

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ pixelBoard.style.width = pixelBoardSize * 40 + 'px';
 
 fillBoard(pixelBoardSize);
 
+/**
+ * source: Consultei o repositório Trybe exercise-end-block5 como base para criação e adaptação da função
+ * Link:https://github.com/tryber/exercise-end-block5
+ * @returns 
+ */
 function createPixel(){
   let pixel = document.createElement('div');
   pixel.classList.add('pixel');
   return pixel; 
 }
 
+/**
+ * Consultei o repositório Trybe exercise-end-block5 como base para criação e adaptação da função
+ * Link:https://github.com/tryber/exercise-end-block5
+ */
 function fillBoard(size){
   for (let line = 0; line < size; line+= 1) {
     for (let column = 0; column < size; column += 1) {
